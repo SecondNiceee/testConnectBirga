ef5b6fb5548445f827f0f3867145ebb6b3cac98b
SecondNiceee <col1596321@gmail.com>
2024-05-08 19:11:50 +0300

    asdas

--- src/store/telegramUserInfo.js
+++ src/store/telegramUserInfo.js
@@ -7,18 +7,19 @@ export const fetchUserInfo = createAsyncThunk(
         let firstName = window.Telegram.WebApp.initDataUnsafe.user.first_name
         let lastName =   window.Telegram.WebApp.initDataUnsafe.user.last_name
         let UserId = window.Telegram.WebApp.initDataUnsafe.user.id
-        let user  = await axios.get( 'https://birga.ywa.su/api/user/findOne' ,  {
-            params : {
-              id : UserId
-            }
-          })
+
+        // let user  = await axios.get( 'https://birga.ywa.su/api/user/findOne' ,  {
+        //     params : {
+        //       id : UserId
+        //     }
+        //   })
 
         // let firstName = 'Привет'
         // let lastName =  'Как дела'
         // let UserId = '14'
         // let photo = 'бла бла фото еб'
 
-        return {firstName : firstName, lastName : lastName , id : UserId , photo : user.photo}
+        return {firstName : firstName, lastName : lastName , id : UserId , photo : 'чукча'}
     }
 
 
