fdc662dd3c616dfd78520c680781c6f0e7408cc5
SecondNiceee <col1596321@gmail.com>
2024-05-11 21:51:14 +0300

    asdas

--- src/pages/MyAds/components/LastAds.jsx
+++ src/pages/MyAds/components/LastAds.jsx
@@ -9,85 +9,68 @@ import WhiteBlock from './WhiteBlock';
 import LastTop from './LastTop';
 import LastImages from './LastImages';
 import LastSertificates from './LastSertificates';
+
+let start;
+let move;
 const LastAds = ({aboutReaction , isClosed , setClosed , openAboutReactionFunc , openAboutReaction}) => {
 
 
 
     const [transform, setTransform] = useState(0)
     const [transition , setTransition] = useState('0.4s')
-    
-    useEffect( () => {
-    
-
-        let start;
-        let move;
-        function startHandler(e){   
-            if (!isClosed && e.target.closest('.bottom__one') === null){
-                setTransition('0s')
-                start = e.touches[0].pageY;
-            }
+    function startHandler(e){   
+        if (!isClosed && e.target.closest('.bottom__one') === null){
+            setTransition('0s')
+            start = e.touches[0].pageY;
         }
-        function moveHandler(e){
-                if (!isClosed && e.target.closest('.bottom__one') === null && e.target.closest('.last-images') === null && e.target.closest('.last-sertificates') === null ){
+    }
+    function moveHandler(e){
+            if (!isClosed && e.target.closest('.bottom__one') === null && e.target.closest('.last-images') === null && e.target.closest('.last-sertificates') === null ){
+
+                move = e.touches[0].pageY;
     
-                    move = e.touches[0].pageY;
-        
-                    if (move - start <= 0){
-                        setTransform(0)
-        
-                    }
-                    else{
-        
-                        setTransform(move - start)
-                    }
+                if (move - start <= 0){
+                    setTransform(0)
+    
                 }
-        }
-        function endHandler(e){
-            if (!isClosed && e.target.closest('.bottom__one') === null  && e.target.closest('.last-images') === null && e.target.closest('.last-sertificates') === null){
-
-                setTransition('0.3s')
-                if (!openAboutReaction){
+                else{
     
-                    if (move - start > 80){
-                        setClosed(true)
-                    }
-                    else{
-                        
-                        setTransform(0)
-                    }
+                    setTransform(move - start)
                 }
-                move = 0;
-                start = 0;
             }
+    }
+    function endHandler(e){
+        if (!isClosed && e.target.closest('.bottom__one') === null  && e.target.closest('.last-images') === null && e.target.closest('.last-sertificates') === null){
+
+            setTransition('0.3s')
+            if (!openAboutReaction){
+
+                if (move - start > 80){
+                    setClosed(true)
+                }
+                else{
+                    
+                    setTransform(0)
+                }
+            }
+            move = 0;
+            start = 0;
         }
-        window.removeEventListener('touchstart' , startHandler)
-        window.removeEventListener('touchmove' , moveHandler)
-        window.removeEventListener('touchend' , endHandler)
-        window.addEventListener('touchstart' , startHandler )
-        window.addEventListener('touchmove' , moveHandler)
-        window.addEventListener('touchend', endHandler )
-        return () => {
-            window.removeEventListener('touchstart' , startHandler)
-            window.removeEventListener('touchmove' , moveHandler)
-            window.removeEventListener('touchend' , endHandler)
-        }
-    }, [] )
+    }
+
 
     
-    useEffect(  () => {
+    // useEffect(  () => {
         
-        if (isClosed){
-            document.querySelector('.aboutOne').style.overflowY = 'scroll'
-            setTransform('100%')
-        }
-        else{
-            // document.querySelector('.aboutOne').style.overflowY = 'clip'
-            // document.querySelector('.MyAdsContainer').style.overflowY = 'clip'
-            // document.querySelector('.MyAdsContainer').style.overflowY = 'clip'
+    //     if (isClosed){
+    //         document.querySelector('.aboutOne').style.overflowY = 'scroll'
+    //         setTransform('100%')
+    //     }
+    //     else{
             
-            setTransform(0)
-        }
-    } , [isClosed ]  )
+    //         setTransform(0)
+    //     }
+    // } , [isClosed ]  )
 
     const style = useMemo( () => {
         
@@ -108,7 +91,10 @@ const LastAds = ({aboutReaction , isClosed , setClosed , openAboutReactionFunc ,
 
 
         <div className={isClosed ? "last-ads" : "last-ads open"} 
-        style={style}
+        style={style} 
+        onTouchMove={moveHandler}
+        onTouchEnd={endHandler}
+        onTouchStart={startHandler}
         >
 
             <WhiteBlock />
