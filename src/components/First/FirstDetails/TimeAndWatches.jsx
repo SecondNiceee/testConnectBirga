3a72d05201545d018be52001ed13f027211b8cff
SecondNiceee <col1596321@gmail.com>
2024-05-21 21:19:46 +0300

    asd

--- src/components/First/FirstDetails/TimeAndWatches.jsx
+++ src/components/First/FirstDetails/TimeAndWatches.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 const TimeAndWatches = ({time , watches}) => {
+
+  var options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour : 'numeric',
+    minute : 'numeric',
+    timezone: 'UTC'
+  };
+
+  
   return (
     <div className="timeAndWatches">
-      <p className="time">{time}</p>
+      <p className="time">{time.toLocaleString("ru", options)}</p>
       <p className="watches">{watches}</p>
     </div>
   );
--- src/components/UI/TextArea/TextArea.jsx
+++ src/components/UI/TextArea/TextArea.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import cl from './TextArea.module.css'
 const TextArea = ({className , value , setValue , ...props}) => {
     return (
-        <textarea spellCheck="false"  autoComplete="off" value={value} onChange = {(e) => {setValue(e.target.value)}} className = {className ? [cl.TextArea , className].join(' ') : cl.TextArea} {...props}></textarea>
+        <textarea  spellCheck="false"  autoComplete="off" value={value} onChange = {(e) => {setValue(e.target.value)}} className = {className ? [cl.TextArea , className].join(' ') : cl.TextArea} {...props}></textarea>
     );
 };
 
