8fa964d49177161f448fc57799d51d1411650553
SecondNiceee <col1596321@gmail.com>
2024-06-08 14:52:27 +0300

    das

--- src/pages/ADCreatingTwo/CatchDate/CatchDate.jsx
+++ src/pages/ADCreatingTwo/CatchDate/CatchDate.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from "react";
 import dateIcon from "../../../images/icons/date.svg";
 import rightArrow from "../../../images/icons/ArrowRight.svg";
 import GreyText from "../../../components/UI/GreyText/GreyText";
-import cl from './CatchDate.module.css'
+import './CatchDate.module.css'
 
 let errorDate = new Date(0)
 const CatchDate = ({ className , whichOne , state, setState,errors,  ...props }) => {
@@ -49,7 +49,7 @@ return (
     {...props}
      style={ whichOne === 'startOnly' ? {} : {transform : getTranslate() }}
       id="dateSwapper"
-      className={className ? [cl.CatchDate, className].join(" ") : cl.CatchDate   }>
+      className={className ? ['CatchDate', className].join(" ") : 'CatchDate'   }>
         {/* <input type="datetime-local" name="startOnly" />
         <input type="datetime" name='start' />
         <input type="datetime" name="end" /> */}
@@ -57,57 +57,57 @@ return (
       onClick = {() => {
         setState({...state, isOpen : true , isSingleOpen : true})
       }} 
-        htmlFor="startOnly" ref={ref1} className={[cl.DateTimePicker , cl.flexStart].join(' ')} 
+        htmlFor="startOnly" ref={ref1} className={['DateTimePicker' , 'flexStart'].join(' ')} 
         style={errors.singleError ? {border : '1px solid red'} : {}}
         >
-        <div className={cl.left}>
-          <img className={cl.leftImage} src={dateIcon} alt="" />
-          <p className={cl.text}>
+        <div className={'left'}>
+          <img className={'leftImage'} src={dateIcon} alt="" />
+          <p className={'text'}>
 
              {state.singleTime ? state.singleTime.toLocaleString("ru", options) : 'Дата и время'  } 
           </p>
         </div>
-        <img src={rightArrow} alt="" className={cl.arrowRight} />
+        <img src={rightArrow} alt="" className={'arrowRight'} />
       </label>
 
-      {/* <div ref={ref2} className={cl.PeriodInput} >
+      <div ref={ref2} className={'PeriodInput'} >
 
-        <GreyText className={cl.myAdsGreyText}> НАЧАТЬ:  </GreyText>
+        <GreyText className={'myAdsGreyText'}> НАЧАТЬ:  </GreyText>
 
         <div 
               onClick = {() => {
                 setState({...state, isOpen : true , isStartOpen : true})
               }} 
-         className={cl.DateTimePicker}   style={errors.startError ? {border : '1px solid red'} : {}} >
-          <div className={cl.left}>
-            <img className={cl.leftImage} src={dateIcon} alt="" />
-            <p className={cl.text}>
+         className={'DateTimePicker'}   style={errors.startError ? {border : '1px solid red'} : {}} >
+          <div className={'left'}>
+            <img className={'leftImage'} src={dateIcon} alt="" />
+            <p className={'text'}>
                {state.startTime ? state.startTime.toLocaleString("ru", options) : 'Дата и время начала'  } 
             </p>
           </div>
-          <img src={rightArrow} alt="" className={cl.arrowRight} />
+          <img src={rightArrow} alt="" className={'arrowRight'} />
         </div>
 
 
 
-        <GreyText className={ cl.myAdsGreyTextTwo} > ЗАКОНЧИТЬ:  </GreyText>
-        <div className={cl.DateTimePicker}
+        <GreyText className={ 'myAdsGreyTextTwo'} > ЗАКОНЧИТЬ:  </GreyText>
+        <div className={'DateTimePicker'}
                       onClick = {() => {
                         setState({...state, isOpen : true , isEndOpen : true})
                       }} 
                       style={errors.endError ? {border : '1px solid red'} : {}}
         >
           
-          <div className={cl.left}>
-            <img className={cl.leftImage} src={dateIcon} alt="" />
-            <p className={cl.text}>
+          <div className={'left'}>
+            <img className={'leftImage'} src={dateIcon} alt="" />
+            <p className={'text'}>
 
             {state.endTime && String(state.endTime) !== String(errorDate) ? state.endTime.toLocaleString("ru", options) : 'Дата и время конца'  } 
             </p>
           </div>
-          <img src={rightArrow} alt="" className={cl.arrowRight} />
+          <img src={rightArrow} alt="" className={'arrowRight'} />
         </div>
-      </div> */}
+      </div>
 
     </div>
   );
