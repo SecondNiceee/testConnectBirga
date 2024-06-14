ff59a4ab450e9c6610e9d831ef65e4302bbddd37
SecondNiceee <col1596321@gmail.com>
2024-06-14 20:25:43 +0300

    dasd

--- src/components/UI/SmallTextarea/SmallTextarea.jsx
+++ src/components/UI/SmallTextarea/SmallTextarea.jsx
@@ -2,18 +2,37 @@ import React, { useEffect, useRef } from "react";
 import cl from "./SmallTextarea.module.css";
 const SmallTextarea = ({ value, setValue, className }) => {
     const textAreaRef = useRef(null)
+    const refTwo = useRef(null)
     useEffect( () => {
         let len = value.split(/\r\n|\r|\n/).length
         // textAreaRef.current.style.height = (12 + 11 + 17.6*len).toString() + 'px'
         console.log(textAreaRef.current.scrollHeight)
         console.log(textAreaRef.current.offsetHeight)
-        if (textAreaRef.current.scrollHeight > textAreaRef.current.offsetHeight ){
-            textAreaRef.current.style.height = (textAreaRef.current.scrollHeight).toString() + 'px'
-        }
+        textAreaRef.current.style.height = (refTwo.current.scrollHeight).toString() + 'px'
+        
 
     } , [value] )
   return (
     <>
+
+        <textarea
+    ref={refTwo}
+        className = {className ? [className, cl.textArea].join(' ') : cl.textAreaHidden}
+      placeholder="Опишите свой опыт и подход к работе"
+      value={value}
+      readOnly = {true}
+      onChange={(e) => {
+        setValue(e.target.value)
+      }}
+      name=""
+      id=""
+    //   onBlur={(e) => {
+    //     let stringWithoutEmptyLines = value.replace(/\s+/g, " ").trim();
+    //     setValue(stringWithoutEmptyLines)
+    //   }}
+    />
+
+
     <textarea
     ref={textAreaRef}
         className = {className ? [className, cl.textArea].join(' ') : cl.textArea}
