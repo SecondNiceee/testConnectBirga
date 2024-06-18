4c7a6b21e9265cb6a46838a546a6559e5fa5d3e9
SecondNiceee <col1596321@gmail.com>
2024-06-18 15:05:00 +0300

    das

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import React from 'react';
+import ImageUploading from 'react-images-uploading';
+
 
 import "./css/Main.css";
 import "./css/Fonts.css";
@@ -160,15 +163,78 @@ function App() {
     dispatch(fetchMyOrders())
   }, []);
 
+
+
+
+
+  const [images, setImages] = React.useState([]);
+  const maxNumber = 69;
+
+  const onChange = (imageList, addUpdateIndex) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+
+
+
+
+  
   return (
-    <BrowserRouter>
-      <div className="UperContainer">
-        <FirstMenu />
-        <div className="MainContainer">
-          <AnimatedSwitch />
-        </div>
-      </div>
-    </BrowserRouter>
+
+<div className="App">
+      <ImageUploading
+        multiple
+        value={images}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          // write your building UI
+          <div className="upload__image-wrapper">
+            <button
+              style={isDragging ? { color: 'red' } : undefined}
+              onClick={onImageUpload}
+              {...dragProps}
+            >
+              Click or Drop here
+            </button>
+            &nbsp;
+            <button onClick={onImageRemoveAll}>Remove all images</button>
+            {imageList.map((image, index) => (
+              <div key={index} className="image-item">
+                <img src={image['data_url']} alt="" width="100" />
+                <div className="image-item__btn-wrapper">
+                  <button onClick={() => onImageUpdate(index)}>Update</button>
+                  <button onClick={() => onImageRemove(index)}>Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </ImageUploading>
+    </div>
+
+
+
+
+    // <BrowserRouter>
+    //   <div className="UperContainer">
+    //     <FirstMenu />
+    //     <div className="MainContainer">
+    //       <AnimatedSwitch />
+    //     </div>
+    //   </div>
+    // </BrowserRouter>
   );
 }
 
