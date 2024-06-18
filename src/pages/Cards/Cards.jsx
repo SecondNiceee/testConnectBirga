8d9411b2aa3095dc2be82bcc9a4ddf7d35324fa5
SecondNiceee <col1596321@gmail.com>
2024-06-18 18:30:17 +0300

    das

--- src/pages/Cards/Cards.jsx
+++ src/pages/Cards/Cards.jsx
@@ -1,167 +1,175 @@
-import React, { useEffect, useState } from 'react';
-import TaskName from '../../components/UI/TaskName/TaskName';
-import DescriptionAndPhoto from '../../components/UI/DescriptionAndPhoto/DescriptionAndPhoto';
-import behanceIcon from '../../images/icons/behance.svg';
-import dripleIcon from '../../images/icons/Dribble.svg';
-import dropfileIcon from '../../images/icons/Dropfile.svg';
-import BackButton from '../../constants/BackButton';
-import MainButton from '../../constants/MainButton';
-import { useDispatch } from 'react-redux';
-import { addCard } from '../../store/profile';
-let localCardSetting
-const Cards = ({setCardsOpen, setAboutU , aboutU}) => {
-    const [cardsSetting , setCardsSetting] = useState({
-        title : '',
-        description : '',
-        photos : [],
-        behanceLink : '',
-        dribbbleLink : '',
-        dropfileLink : ''
-    })
-    const dispatch = useDispatch()
-    const [errors, setErrors] = useState({
-        nameError : false,
-        fileError : false
-    })
-    localCardSetting = cardsSetting
+import React, { useEffect, useState } from "react";
+import TaskName from "../../components/UI/TaskName/TaskName";
+import DescriptionAndPhoto from "../../components/UI/DescriptionAndPhoto/DescriptionAndPhoto";
+import behanceIcon from "../../images/icons/behance.svg";
+import dripleIcon from "../../images/icons/Dribble.svg";
+import dropfileIcon from "../../images/icons/Dropfile.svg";
+import BackButton from "../../constants/BackButton";
+import MainButton from "../../constants/MainButton";
+import { useDispatch } from "react-redux";
+import { addCard } from "../../store/profile";
+let localCardSetting;
+const Cards = ({ setCardsOpen, setAboutU, aboutU }) => {
+  const [cardsSetting, setCardsSetting] = useState({
+    title: "",
+    description: "",
+    photos: [],
+    behanceLink: "",
+    dribbbleLink: "",
+    dropfileLink: "",
+  });
+  const dispatch = useDispatch();
+  const [errors, setErrors] = useState({
+    nameError: false,
+    fileError: false,
+  });
 
-    function checkMistakes(){
-        let fileError = false
-        let titleError = false
-
-        
-        if (localCardSetting.title.length < 3){
-            titleError = true
-        }
-        if (localCardSetting.photos.length < 1){
-            fileError = true
-        }
-        setErrors({fileError : fileError , nameError : titleError})
-        let localErrors = {fileError : fileError , nameError : titleError}
-
-        return Object.values(localErrors).every(value => value === false)
+  useEffect(() => {
+    if (!Object.values(errors).every((value) => value === false)) {
+      let photos = false;
+      let title = false;
+      if (cardsSetting.title.length < 3) {
+        title = true;
+      }
+      if (cardsSetting.photos.length < 1) {
+        photos = true;
+      }
+      let localErrors = { nameError: title, fileError: photos };
+      if (JSON.stringify({ localErrors }) !== JSON.stringify(errors)) {
+        setErrors(localErrors);
+      }
     }
-    function saveFunc(){
-            if (checkMistakes()){
-                console.log('попал сюда..')
-                setAboutU({...aboutU, cards : [...aboutU.cards , localCardSetting] })
-                dispatch(addCard(localCardSetting))
-                document.documentElement.style.overflow = 'auto'
-                setCardsOpen(false)
-            }
-            console.log('я тут')
-            
+  }, [cardsSetting.title, cardsSetting.photos]);
+
+  localCardSetting = cardsSetting;
+
+  function checkMistakes() {
+    let fileError = false;
+    let titleError = false;
+
+    if (localCardSetting.title.length < 3) {
+      titleError = true;
     }
+    if (localCardSetting.photos.length < 1) {
+      fileError = true;
+    }
+    setErrors({ fileError: fileError, nameError: titleError });
+    let localErrors = { fileError: fileError, nameError: titleError };
 
-    MainButton.show()
-    MainButton.setText('Добавить кейс')
-    MainButton.onClick(saveFunc)
-    BackButton.show()
-    BackButton.onClick(backFunc)
+    return Object.values(localErrors).every((value) => value === false);
+  }
+  function saveFunc() {
+    if (checkMistakes()) {
+      console.log("попал сюда..");
+      setAboutU({ ...aboutU, cards: [...aboutU.cards, localCardSetting] });
+      dispatch(addCard(localCardSetting));
+      document.documentElement.style.overflow = "auto";
+      setCardsOpen(false);
+    }
+    console.log("я тут");
+  }
 
+  MainButton.show();
+  MainButton.setText("Добавить кейс");
+  MainButton.onClick(saveFunc);
+  BackButton.show();
+  BackButton.onClick(backFunc);
 
-    function backFunc(){
-        window.Telegram.WebApp
-        .showPopup({
-          title: "Создать?",
-          message: "Вы хотите создать этот кейс?",
-          buttons: [
-            { id: "save", type: "default", text: "Да" },
-            { id: "delete", type: "destructive", text: "Нет" },
-          ],
-        } , (buttonId) => {
-
-          if (buttonId === "delete" || buttonId === null) {
-            setCardsOpen(false)
+  function backFunc() {
+    window.Telegram.WebApp.showPopup(
+      {
+        title: "Создать?",
+        message: "Вы хотите создать этот кейс?",
+        buttons: [
+          { id: "save", type: "default", text: "Да" },
+          { id: "delete", type: "destructive", text: "Нет" },
+        ],
+      },
+      (buttonId) => {
+        if (buttonId === "delete" || buttonId === null) {
+          setCardsOpen(false);
+        }
+        if (buttonId === "save") {
+          if (!checkMistakes()) {
+            setAboutU({
+              ...aboutU,
+              cards: [...aboutU.cards, localCardSetting],
+            });
+            dispatch(addCard(localCardSetting));
+            document.documentElement.style.overflow = "auto";
+            setCardsOpen(false);
           }
-          if (buttonId === "save") {
-            if (!checkMistakes()){
-                setAboutU({...aboutU, cards : [...aboutU.cards , localCardSetting] })
-                dispatch(addCard(localCardSetting))
-                document.documentElement.style.overflow = 'auto'
-                setCardsOpen(false)
-            }
-        
-            
-          }
-
-
-        } )
-
-    }
-
-    useEffect(  () => {
-        return () => {
-            MainButton.hide()
-            BackButton.offClick(backFunc)
         }
-    } , [] )
-    return (
-        <div className='cards'>
+      }
+    );
+  }
 
+  useEffect(() => {
+    return () => {
+      MainButton.hide();
+      BackButton.offClick(backFunc);
+    };
+  }, []);
+  return (
+    <div className="cards">
+      <h3 className="cards-title">Новый кейс</h3>
 
-            <h3 className='cards-title'>Новый кейс</h3>
-
-            {/* <button onClick={() => {
+      {/* <button onClick={() => {
                                 setAboutU({...aboutU, cards : [...aboutU.cards , cardsSetting] })
                                 dispatch(addCard(cardsSetting))
                                 setCardsOpen(false)
 
             }}>Сохранить</button> */}
-            <TaskName 
-            placeholder={'Придумайте название для нового кейса'}
-            className={'cards-taskName'}
-            title={'НАЗВАНИЕ КЕЙСА'}
-            text={cardsSetting.title}
-            description = {cardsSetting.description}
-            setText={(e) => {
-                setCardsSetting({...cardsSetting , title : e})
-            }}
-            errorValue={errors.nameError}
-            
-            underText={''}
-             />
+      <TaskName
+        placeholder={"Придумайте название для нового кейса"}
+        className={"cards-taskName"}
+        title={"НАЗВАНИЕ КЕЙСА"}
+        text={cardsSetting.title}
+        description={cardsSetting.description}
+        setText={(e) => {
+          setCardsSetting({ ...cardsSetting, title: e });
+        }}
+        errorValue={errors.nameError}
+        underText={""}
+      />
 
-             
+      <DescriptionAndPhoto
+        fileError={errors.fileError}
+        className={"cards-descriptionAndPhoto"}
+        text={cardsSetting.description}
+        textPlaceholder={"Опишите в чем особенность ваших работ"}
+        setText={(e) => {
+          setCardsSetting({ ...cardsSetting, description: e });
+        }}
+        setPhotos={(e) => {
+          setCardsSetting({ ...cardsSetting, photos: e });
+        }}
+        photos={cardsSetting.photos}
+        MyInformation={false}
+        textTitle={"ОПИСАНИЕ КЕЙСА"}
+        filesTitle={""}
+      />
+      <button onClick={saveFunc}>Сохранить</button>
+      <p className="cards-underText">
+        Расскажите о себе и своем опыте работы Прикрепите релевантные примеры
+      </p>
 
-
-            <DescriptionAndPhoto
-                fileError={errors.fileError}
-                className={'cards-descriptionAndPhoto'}
-                text={cardsSetting.description}
-                textPlaceholder={'Опишите в чем особенность ваших работ'}
-                setText={(e) => {
-                    setCardsSetting({...cardsSetting , description : e})
-                }}
-                setPhotos={(e) => {
-                    setCardsSetting({...cardsSetting, photos : e})
-                }}
-                photos={cardsSetting.photos}
-                MyInformation={false}
-                textTitle={'ОПИСАНИЕ КЕЙСА'}
-                filesTitle={''}            
-            />
-            <button 
-            onClick={saveFunc}>Сохранить</button>
-            <p className='cards-underText'>Расскажите о себе и своем опыте работы
-Прикрепите релевантные примеры</p>
-
-            <div className="cards-links">
-                <div className="behans-link cards-link">
-                    <img src={behanceIcon} alt="" />
-                    <p>Ссылка на Behance</p>
-                </div>
-                <div className="behans-link cards-link">
-                    <img src={dripleIcon} alt="" />
-                    <p>Ссылка на Dribbble</p>
-                </div>
-                <div className="behans-link cards-link">
-                    <img src={dropfileIcon} alt="" />
-                    <p>Ссылка на Dropfile</p>
-                </div>
-            </div>
+      <div className="cards-links">
+        <div className="behans-link cards-link">
+          <img src={behanceIcon} alt="" />
+          <p>Ссылка на Behance</p>
         </div>
-    );
+        <div className="behans-link cards-link">
+          <img src={dripleIcon} alt="" />
+          <p>Ссылка на Dribbble</p>
+        </div>
+        <div className="behans-link cards-link">
+          <img src={dropfileIcon} alt="" />
+          <p>Ссылка на Dropfile</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
