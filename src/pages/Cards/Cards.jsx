dc4f19b659e4cf4972c127888c4db6b39677209f
SecondNiceee <col1596321@gmail.com>
2024-06-23 22:22:28 +0300

    das

--- src/pages/Cards/Cards.jsx
+++ src/pages/Cards/Cards.jsx
@@ -127,6 +127,7 @@ const Cards = ({ setCardsOpen, setAboutU, aboutU , save  }) => {
 
 
       let myFormData = new FormData()
+      console.log(localCardSetting)
       myFormData.append("categoryId", localCardSetting.category.id)
       myFormData.append("title" , localCardSetting.title)
       myFormData.append("description" , localCardSetting.description)
