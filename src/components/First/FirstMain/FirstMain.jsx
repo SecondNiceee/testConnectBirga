7ab07ea75ecd81c819be80b820bcff9359eb6230
SecondNiceee <col1596321@gmail.com>
2024-07-04 23:39:24 +0300

    dasda

--- src/components/First/FirstMain/FirstMain.jsx
+++ src/components/First/FirstMain/FirstMain.jsx
@@ -2,13 +2,14 @@ import React, { memo, useCallback } from "react";
 import FirstBlock from "./FirstBlock";
 import { useDispatch, useSelector } from "react-redux";
 import { addWatch } from "../../../store/watchedAds";
+
 const FirstMain = ({ ordersInformation, setDetailsActive , ...props}) => {
   const watchedArr = useSelector(state => state.watchedAds.watchedAds)
   const dispatch = useDispatch()
   console.log('РЕНДЕР')
   const setDetailsActiveF = useCallback( (e,i) => {
     setDetailsActive({isOpen : true , id : i})
-    // dispatch(addWatch(e.id))
+    dispatch(addWatch(e.id))
   } , [] )
   return (
     <div {...props} className="FirstMain">
