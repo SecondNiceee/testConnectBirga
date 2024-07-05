ba4089d8934248afc0147f16a892f19d554b8133
SecondNiceee <col1596321@gmail.com>
2024-07-05 18:59:11 +0300

    das

--- src/components/UI/FullPicker/FullPicker.jsx
+++ src/components/UI/FullPicker/FullPicker.jsx
@@ -1,38 +1,72 @@
-import React, {  useMemo} from 'react';
-import cl from './FullPicker.module.css'
-const FullPicker = ({values , keys , nowKey , setNowKey, className , GreyIntWidth, GreyWidth }) => {
+import React, { useMemo } from "react";
+import cl from "./FullPicker.module.css";
+const FullPicker = ({
+  values,
+  keys,
+  nowKey,
+  setNowKey,
+  className,
+  GreyIntWidth,
+  GreyWidth,
+}) => {
+  console.log(nowKey, keys);
+  const myTransform = useMemo(() => {
+    for (let i = 0; i < keys.length; i++) {
+      if (nowKey === keys[0]) {
+        return "translateX(2px)";
+      }
+      if (nowKey === keys[i]) {
+        return "translateX(" + (GreyIntWidth * i + 2).toString() + "px)";
+      }
+    }
+  }, [nowKey, GreyIntWidth, keys]);
+  return (
+    <div className={className ? [cl.track, className].join(" ") : cl.track}>
+      <div
+        style={{ width: GreyWidth, transform: myTransform }}
+        className={cl.greyBlock}
+      ></div>
+      {nowKey === keys[0] ? (
+        <p
+          className={cl.bigValue}
+          onClick={(e) => {
+            setNowKey(keys[0]);
+          }}
+        >
+          {values[0]}
+        </p>
+      ) : (
+        <p
+          className={cl.value}
+          onClick={(e) => {
+            setNowKey(keys[0]);
+          }}
+        >
+          {values[0]}
+        </p>
+      )}
 
-
-    console.log(nowKey , keys)
-    const myTransform = useMemo( () => {
-        for (let i = 0; i < keys.length ; i++ ){
-            if (nowKey === keys[0]){
-                return 'translateX(2px)'
-            }
-            if (nowKey === keys[i]){
-                return 'translateX(' + ((GreyIntWidth*i) + 2).toString() + 'px)'
-            }
-        }
-    } , [nowKey, GreyIntWidth, keys ] )
-    return (
-        <div   className={className ? [cl.track , className].join(' ') : cl.track}>
-            <div style={{width : GreyWidth, transform : myTransform}} className={cl.greyBlock}></div>
-                <p style={ nowKey === keys[0] ?  {zIndex : 10, fontWeight : 600} : {zIndex : 10}
-                }   className={cl.value} onClick={(e) => {  
-                                setNowKey(keys[0]) 
-                            } 
-                            } > 
-                            {values[0]}
-                    </p>
-                <p style={ nowKey === keys[1] ?  {zIndex : 10, fontWeight : 600} : {zIndex : 10}
-                }   className={cl.value} onClick={(e) => {
-                            setNowKey(keys[1]) 
-                        } 
-                        } >
-                        {values[1]}
-                </p>
-        </div>
-    );
+      {nowKey === keys[1] ? (
+        <p
+          className={cl.bigValue}
+          onClick={(e) => {
+            setNowKey(keys[1]);
+          }}
+        >
+          {values[1]}
+        </p>
+      ) : (
+        <p
+          className={cl.value}
+          onClick={(e) => {
+            setNowKey(keys[1]);
+          }}
+        >
+          {values[1]}
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default FullPicker;
