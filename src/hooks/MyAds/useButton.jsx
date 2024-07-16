99fc8390169078b4cb3bace6d14a7934b1ccb8d0
SecondNiceee <col1596321@gmail.com>
2024-07-17 02:00:22 +0300

    da

--- src/hooks/MyAds/useButton.jsx
+++ src/hooks/MyAds/useButton.jsx
@@ -31,7 +31,7 @@ export const useButton = ({
         title: "Внимание",
         message: "Перед выбором исполнителя ознакомьтесь с FAQ Биржи.",
         buttons: [
-          { id: "save", type: "OK", text: "Ознакомиться" },
+          { id: "save", type: "ok", text: "Ознакомиться" },
           { id: "delete", type: "default", text: "Продолжить" },
         ],
       } , (buttonId) => {
@@ -41,7 +41,7 @@ export const useButton = ({
             title : "Выбрать?",
             message : "Вы уверены, что хотите выбрать этого исполнителя?",
             buttons : [
-              { id: "save", type: "OK", text: "Да" },
+              { id: "save", type: "ok", text: "Да" },
               { id: "delete", type: "default", text: "Нет" },
             ]
           } , (buttonId) => {
