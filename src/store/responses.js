cf9e84aeb02f834a46eb1d2a0363826ac3ca68d2
SecondNiceee <col1596321@gmail.com>
2024-07-23 12:57:45 +0300

    dsa

--- src/store/responses.js
+++ src/store/responses.js
@@ -24,7 +24,7 @@ export const addResponse = createAsyncThunk(
     "addResponse" , 
     async function (par){
         try{
-            for (let i = 0 ; i < 10;i++){
+            for (let i = 0 ; i < 40;i++){
                 await axios.post("https://back-birga.ywa.su/response" , par[0], {
                     params : {
                         advertisementId : par[1].advertisement.id,
