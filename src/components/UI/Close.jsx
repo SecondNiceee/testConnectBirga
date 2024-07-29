750a6712917d04300fd654f7cc7346b163ad5040
SecondNiceee <col1596321@gmail.com>
2024-07-29 22:24:45 +0300

    dsa

--- src/components/UI/Close.jsx
+++ src/components/UI/Close.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { changeMenuActive } from '../../store/menuSlice';
+import { useDispatch } from 'react-redux';
 
 
-const Close = ({isMenuActive , setMenuActive}) => {
+const Close = ({isMenuActive }) => {
+    const dispatch = useDispatch()
+    const setMenuActive = useCallback(
+        (set) => {
+
+            const menu = document.documentElement.querySelector(".FirstMenu");
+            menu.style.transition = "0.4s";
+            menu.style.transform = "translateX(0px)";
+            menu.style.left = "-301px";
+          dispatch(changeMenuActive(set));
+
+        },
+        [dispatch]
+      );
     return (
         
             <div  onClick = {() => { setMenuActive(false)}  } className='close' >
