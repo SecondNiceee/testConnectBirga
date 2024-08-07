9416ed6a454589e4065d4524d29368431a1070ea
SecondNiceee <col1596321@gmail.com>
2024-08-07 22:02:36 +0300

    das

--- src/components/UI/MyButton/MyButton.jsx
+++ src/components/UI/MyButton/MyButton.jsx
@@ -3,9 +3,13 @@ import classes from  "./MyButton.module.css"
 const MyButton = (props , className) => {
     const myRef = useRef(null)
     const vibrate = useCallback( () => {
-        window.navigator.vibrate(20);
+        window.navigator.vibrate(70);
         myRef.current.style.backgroundColor = "white"
         myRef.current.style.color = "black"
+        setTimeout( () => {
+        myRef.current.style.backgroundColor = "rgb(46, 165, 255)"
+         myRef.current.style.color = "white"
+        } , 1000 )
     }  , [])
     const clickHandler = useCallback( (e) => {
         myRef.current.style.backgroundColor = "white"
