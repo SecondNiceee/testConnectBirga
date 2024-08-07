3b69859ef8e99632ecc2d20db0bbfaeffc7f45e6
SecondNiceee <col1596321@gmail.com>
2024-08-07 21:54:26 +0300

    das

--- src/components/UI/MyButton/MyButton.jsx
+++ src/components/UI/MyButton/MyButton.jsx
@@ -2,17 +2,20 @@ import React, { useCallback, useRef } from 'react';
 import classes from  "./MyButton.module.css"
 const MyButton = (props , className) => {
     const myRef = useRef(null)
+    const vibrate = useCallback( () => {
+        window.navigator.vibrate(100);
+    }  , [])
     const clickHandler = useCallback( (e) => {
         myRef.current.style.backgroundColor = "white"
         myRef.current.style.color = "black"
-        window.navigator.vibrate(1000);
+        
     }  , [])
     const touchEnd = useCallback( (e) => {
         myRef.current.style.backgroundColor = "rgb(46, 165, 255)"
          myRef.current.style.color = "white"
     } )
     return ( 
-        <div onTouchEnd={touchEnd} onTouchStart={clickHandler}>
+        <div onClick={vibrate} onTouchEnd={touchEnd} onTouchStart={clickHandler}>
             <button ref={myRef} className={ className ? [classes.MyButton, className].join(' ') : classes.MyButton } {...props}>{props.children}</button>
         </div>
     );
