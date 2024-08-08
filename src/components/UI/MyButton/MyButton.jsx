d761ac4490b8e630194c9901230fbb083deafc2e
SecondNiceee <col1596321@gmail.com>
2024-08-08 15:16:11 +0300

    das

--- src/components/UI/MyButton/MyButton.jsx
+++ src/components/UI/MyButton/MyButton.jsx
@@ -1,10 +1,10 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import classes from  "./MyButton.module.css"
 const MyButton = (props , className) => {
     const myRef = useRef(null)
     const vibrate = useCallback( () => {
         // window.navigator.vibrate(100);
-        window.Telegram.WebApp.HapticFeedback.impactOccurred('medium')
+        window.Telegram.WebApp.HapticFeedback.selectionChanged()
         myRef.current.style.backgroundColor = "#47A2E7"
         myRef.current.style.color = "#E6E6E7"
         setTimeout( () => {
@@ -13,6 +13,7 @@ const MyButton = (props , className) => {
         } , 100 )
     }  , [])
     const clickHandler = useCallback( (e) => {
+        
         myRef.current.style.backgroundColor = "#47A2E7"
         myRef.current.style.color = "#E6E6E7"
         
@@ -22,7 +23,9 @@ const MyButton = (props , className) => {
          myRef.current.style.color = "white"
     } , [] )
     return ( 
-        <div  onClick={vibrate} onTouchEnd={touchEnd} onTouchStart={clickHandler} {...props} >
+        <div  onClick={() => {
+            window.Telegram.WebApp.HapticFeedback.selectionChanged()
+            vibrate()}} onTouchEnd={touchEnd} onTouchStart={clickHandler} {...props} >
             <button ref={myRef} className={ className ? [classes.MyButton, className].join(' ') : classes.MyButton } {...props}>{props.children}</button>
         </div>
     );
