4111c4b8a5e4cbfc8b99a9aead2876d1ea014c5d
SecondNiceee <col1596321@gmail.com>
2024-08-09 04:28:04 +0300

    dsa

--- src/components/First/FirstMain/FirstMainTop.jsx
+++ src/components/First/FirstMain/FirstMainTop.jsx
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React, { memo, useEffect } from "react";
 import Pallete from "../../UI/Pallete/Pallete";
 import ShareIcon from "../../UI/ShareIcon/ShareIcon";
 
