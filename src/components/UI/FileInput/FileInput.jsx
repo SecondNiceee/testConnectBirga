2473bc3bdde1ba3af1df40a8c6e2c0224c40b168
SecondNiceee <col1596321@gmail.com>
2024-08-11 16:29:17 +0300

    das

--- src/components/UI/FileInput/FileInput.jsx
+++ src/components/UI/FileInput/FileInput.jsx
@@ -25,7 +25,7 @@ const FileInput = ({
       let localImages = [];
       let localFiles = []
       newFiles.forEach((event) => {
-        resizeImage(event, 1000, 1000, 1).then((value) => {
+        resizeImage(event, 1500, 1500, 1).then((value) => {
           // reader.readAsDataURL(value);
           localImages.push(URL.createObjectURL(value));
           localFiles.push(value)
