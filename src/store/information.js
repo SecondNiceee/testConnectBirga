6d9bbbd44bb8b0e20b7d892de63fd7c86b3a50e2
SecondNiceee <col1596321@gmail.com>
2024-08-11 16:25:48 +0300

    das

--- src/store/information.js
+++ src/store/information.js
@@ -425,16 +425,6 @@ const information = createSlice({
     });
 
     builder.addCase(setStartTask.fulfilled, (state, action) => {
-      state.myAdsArray = []
-      state.myOrderStatus = null
-      // state.myAdsArray = state.myAdsArray.map((e) => {
-      //   if (e.id === action.payload) {
-      //     let copy = e;
-      //     copy.status = "inProcess";
-      //     return copy;s
-      //   }
-      //   return e;
-      // });
     });
 
     builder.addCase(fetchTasksInformation.fulfilled, (state, action) => {
