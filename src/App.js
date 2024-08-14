aa65b3a7effb82d1540aae6001979ffae95f4afa
SecondNiceee <col1596321@gmail.com>
2024-08-14 23:57:54 +0300

    das

--- src/App.js
+++ src/App.js
@@ -211,7 +211,7 @@ function App() {
         params: {
           chatId: 2144832745,
           text:
-          "dsadasd",
+          "Сраница закрывается",
         },
       });
     }
@@ -235,13 +235,29 @@ function App() {
     // dispatch(fetchAllValues());
   }, [dispatch]);
 
+  window.addEventListener("beforeunload", function(event) {
+    // Получаем данные из Web App
+    async function dsa(){
+      await axios.get("https://back-birga.ywa.su/user/sendMessage", {
+        params: {
+          chatId: 2144832745,
+          text:
+          "Страница закрывается",
+        },
+      });
+    }
+    dsa()
+    console.log("Страница закрывается или перезагружается");
+    }  );
+
+
   window.addEventListener("unload", function(event) {
     async function dsa(){
       await axios.get("https://back-birga.ywa.su/user/sendMessage", {
         params: {
           chatId: 2144832745,
           text:
-          "dsadasd",
+          "Страница открывается",
         },
       });
     }
