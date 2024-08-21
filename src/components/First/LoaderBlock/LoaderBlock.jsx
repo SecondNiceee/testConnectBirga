80ee7147a626226892b26340370cdbd3e7f0bc9f
SecondNiceee <col1596321@gmail.com>
2024-08-22 02:18:17 +0300

    dsa

--- src/components/First/LoaderBlock/LoaderBlock.jsx
+++ src/components/First/LoaderBlock/LoaderBlock.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import cl from "./LoaderBlock.module.css"
-import MyLoader from '../../UI/MyLoader/MyLoader';
+import React from "react";
+import cl from "./LoaderBlock.module.css";
+import MyLoader from "../../UI/MyLoader/MyLoader";
 const LoaderBlock = () => {
-    return (
-        <div className={cl.wrapper}>
-            <MyLoader style = {{width : "100vw" , height : "100vh" } }  />
-        </div>
-    );
+  return (
+    <>
+      <div className={cl.wrapper}></div>
+      <MyLoader style={{ width: "100vw", height: "100vh", zIndex : "2000" }} />
+    </>
+  );
 };
 
 export default LoaderBlock;
