281c0b8651aa9aa46bf568053ecf87593eb6387e
SecondNiceee <col1596321@gmail.com>
2024-09-08 17:52:54 +0300

    dsa

--- src/pages/AdCreating.jsx
+++ src/pages/AdCreating.jsx
@@ -293,17 +293,17 @@ const translateText = translation("Вы уверены, что хотите со
       if (spet === 2){
         if (mainRef.current){
           mainRef.current.style.height = "100vh"
-          mainRef.current.style.paddingBottom = "60px"
+          mainRef.current.style.paddingBottom = "76px"
         }
       }
       else{
         if (mainRef.current){
           if (spet === 1){
-            mainRef.current.style.height = "100%"
+            mainRef.current.style.height = "100vh"
             mainRef.current.style.paddingBottom = "0px"
           }
           else{
-            mainRef.current.style.paddingBottom = "60px"
+            mainRef.current.style.paddingBottom = "76px"
             mainRef.current.style.height = "100%"
           }
         }
