82b7f507293a773f533e3fd5e91237c3d9b9840d
SecondNiceee <col1596321@gmail.com>
2024-09-11 20:01:34 +0300

    dsa

--- src/pages/Wallet/components/Stages.jsx
+++ src/pages/Wallet/components/Stages.jsx
@@ -1,21 +1,33 @@
-import React, { memo } from 'react';
-import cl from "../index.module.scss"
-import StageThree from './StageThree';
-import StageOne from './StageOne';
-import StageTwo from './StageTwo';
-const Stages = ({address}) => {
-    return (
-        <div className={cl.stages}>
+import React, { memo } from "react";
+import cl from "../index.module.scss";
+import StageThree from "./StageThree";
+import StageOne from "./StageOne";
+import StageTwo from "./StageTwo";
+const Stages = ({ address }) => {
+  return (
+    <div  className={cl.stages}>
+      <svg
+        className={cl.lineTwo}
+        width="2"
+        height="201"
+        viewBox="0 0 2 201"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1 0.90625L1 200.906"
+          stroke="#4A6275"
+          stroke-linejoin="round"
+          stroke-dasharray="4 4"
+        />
+      </svg>
+      <StageOne address={address} />
 
-                <StageOne address={address} />
+      <StageTwo />
 
-                <StageTwo />
-
-
-                <StageThree />
-
-        </div>
-    );
+      <StageThree />
+    </div>
+  );
 };
 
 export default memo(Stages);
