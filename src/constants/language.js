f5ed6b8cec85a455e684b41457ea9e1efae2483d
SecondNiceee <col1596321@gmail.com>
2024-09-22 12:51:18 +0300

    dsa

--- src/constants/language.js
+++ src/constants/language.js
@@ -1,3 +1,3 @@
 // const en =  window.Telegram.WebApp.initDataUnsafe.user ? window.Telegram.WebApp.initDataUnsafe.user.language_code !== "ru" : false
-const en = true
+const en = false
 export default en
