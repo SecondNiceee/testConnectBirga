168849e6c92ccb1766db8e3179a768197a166f5e
SecondNiceee <col1596321@gmail.com>
2024-09-22 19:50:25 +0300

    dsa

--- src/pages/WalletEnter/components/ErrorBlock/ErrorBlock.jsx
+++ src/pages/WalletEnter/components/ErrorBlock/ErrorBlock.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import cl from "./ErrorBlock.module.scss"
+import Text from '../../../../components/Text/Text';
 const ErrorBlock = () => {
     return (
         <div className={cl.container}>
-            <p className={cl.text}>Некорректная сид-фраза. Проверьте ее и введите снова</p>
+            <Text className={cl.text}>Некорректная сид-фраза. Проверьте ее и введите снова</Text>
         </div>
     );
 };
