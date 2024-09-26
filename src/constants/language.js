e17f09b34fe592dce5470e2565dfcc231b7f01c3
SecondNiceee <col1596321@gmail.com>
2024-09-26 12:13:51 +0300

    dsa

--- src/constants/language.js
+++ src/constants/language.js
@@ -1,5 +1,5 @@
-// const en =  window.Telegram.WebApp.initDataUnsafe.user ? window.Telegram.WebApp.initDataUnsafe.user.language_code !== "ru" : false
-const en = false
+const en =  window.Telegram.WebApp.initDataUnsafe.user ? window.Telegram.WebApp.initDataUnsafe.user.language_code !== "ru" : false
+// const en = false
 // alert(window.Telegram.WebApp.initDataUnsafe.user.language_code )
 console.log('====================================');
 console.log("Хай");
