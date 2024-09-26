a855c101698a69234df6fee80b7f5e4017686930
SecondNiceee <col1596321@gmail.com>
2024-09-26 23:07:42 +0300

    das

---
+++ BadPage/index.js
@@ -0,0 +1,9 @@
+const animation = lottie.loadAnimation({
+    container: document.getElementById('lottie-animation'), // контейнер для анимации
+    renderer: 'svg', // тип рендерера (может быть 'svg', 'canvas' или 'html')
+    loop: true, // зацикливание анимации
+    autoplay: true, // автоматический запуск анимации
+    path: './animation/sorry.json' // путь к вашему JSON-файлу с анимацией
+  });
+sec
+animation.play()
