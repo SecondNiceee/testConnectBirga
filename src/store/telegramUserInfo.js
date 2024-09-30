b4a3a5ce905720a531caf5568cada519fa4418ba
SecondNiceee <col1596321@gmail.com>
2024-09-30 15:09:37 +0300

    das

--- src/store/telegramUserInfo.js
+++ src/store/telegramUserInfo.js
@@ -243,7 +243,7 @@ export const fetchUserInfo = createAsyncThunk(
                 }
             } )
 
-            photoUrl = response.data  ? response.data : null
+            photoUrl = response.data  ? response.data : ""
         }
         return ( {
             firstName: firstName,
