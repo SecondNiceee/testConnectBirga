0cff2b5b339b406c1f0b21dd2031c0e0806d6a2c
SecondNiceee <col1596321@gmail.com>
2024-11-22 10:07:41 +0300

    dsa

--- src/functions/shareFunction.js
+++ src/functions/shareFunction.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 
 export const shareFunction = (id) => async () => {
+
+
     const repsonse = await axios.post(`${process.env.REACT_APP_HOST}/bot/getMailingMessage` , {
       "advertisementId" : id,
       "userId" : Number(window.Telegram.WebApp.initDataUnsafe.user.id)
@@ -10,8 +12,12 @@ export const shareFunction = (id) => async () => {
             "X-API-KEY-AUTH" : process.env.REACT_APP_API_KEY
         }
     })
+    console.log(repsonse)
     const messageId = repsonse.data
+    console.log(messageId)
     window.Telegram.WebApp.shareMessage({
         prepared_message_id: messageId // подставьте полученный prepared_message_id
       });
-  }
+
+
+}
