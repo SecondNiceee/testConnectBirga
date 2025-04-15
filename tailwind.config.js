36f8b669b662d16c5317f12bca73c8871dcfa7e5
SecondNiceee <col1596321@gmail.com>
2025-04-15 22:07:33 +0300

    ds

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sf-pro-display-400' : ['SF Pro Display 400'],
+        'sf-pro-display-400' : ["'SF Pro Display 400'"],
         'sf-pro-display': ['SF Pro Display', 'sans-serif'],
         "sf-compact-rounded" : ['Sf Compact Rounded' , 'sans-serif'],
         "sf-pro-text" : ['SF Pro Text' , 'sans-serif']
