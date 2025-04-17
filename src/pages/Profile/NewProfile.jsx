fab9aed582d1ae92ba333fea2dcc447abdb9a8a1
SecondNiceee <col1596321@gmail.com>
2025-04-18 00:14:55 +0300

    dsa

--- src/pages/Profile/NewProfile.jsx
+++ src/pages/Profile/NewProfile.jsx
@@ -10,7 +10,7 @@ const NewProfile = () => {
   const optionsConfig = useGetOptionsConfig();
 
   return (
-    <div className="pt-[20px] px-[20px] bg-[#18222d] gap-[16px] flex flex-col">
+    <div className="pt-[20px] px-[20px] bg-[#18222d] gap-[16px] flex flex-col h-[100vh] overflow-y-scroll">
 
       <NewProfileCup />
       <PayBlock className="pay-block" />
