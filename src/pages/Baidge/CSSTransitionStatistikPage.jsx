5d49ed9d754109b163e0c8c3d34a92420db9354f
SecondNiceee <col1596321@gmail.com>
2025-04-26 21:36:48 +0300

    aa

--- src/pages/Baidge/CSSTransitionStatistikPage.jsx
+++ src/pages/Baidge/CSSTransitionStatistikPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
-import Baidge from './Baidge';
 import "../../styles/left-right.css"
 import StatisticPage from '../StatisticPage/StatisticPage';
 
--- src/pages/BaidgeCreating/BaidgeCreating.jsx
+++ src/pages/BaidgeCreating/BaidgeCreating.jsx
@@ -1,117 +1,154 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getProfessions } from '../../store/profession';
-import MyLoader from '../../components/UI/MyLoader/MyLoader';
-import BaidgeCreaitingOne from './BaidgeCreaitingOne';
-import BaidgeCreatingTwo from './BaidgeCreatingTwo';
-import { useNavigate } from 'react-router';
-import BaidgeButtonConroller from './hooks/BaidgeButton.conroller';
-import MainButton from '../../constants/MainButton';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProfessions } from "../../store/profession";
+import MyLoader from "../../components/UI/MyLoader/MyLoader";
+import BaidgeCreaitingOne from "./BaidgeCreaitingOne";
+import BaidgeCreatingTwo from "./BaidgeCreatingTwo";
+import { useNavigate } from "react-router";
+import BaidgeButtonConroller from "./hooks/BaidgeButton.conroller";
+import MainButton from "../../constants/MainButton";
 import BackButton from "../../constants/BackButton";
-import menuController from '../../functions/menuController';
-
+import menuController from "../../functions/menuController";
 
 const BaidgeCreating = () => {
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(getProfessions());
+  }, [dispatch]);
 
-    const dispatch = useDispatch();
+  const categorys = useSelector((state) => state.categorys.category);
 
-    useEffect( () => {
-        dispatch(getProfessions())
-    } , [dispatch] )
+  const professions = useSelector((state) => state.profession.professions);
 
+  console.log(professions);
 
-    const categorys = useSelector((state) => state.categorys.category)
+  const [description, setDescription] = useState("");
 
-    const professions = useSelector((state) => state.profession.professions)
+  const taskInformation = {
+    category: categorys[0],
+    subCategory: { subCategory: "Привет" },
+  };
 
-    console.log(professions);
+  const [taggsText, setTaggsText] = useState("");
 
-    const [description, setDescription] = useState("")
+  const [taggs, setTaggs] = useState([]);
 
-    const taskInformation = {category : categorys[0], subCategory : {subCategory : "Привет"}}
+  const [links, setLinks] = useState([""]);
 
-    const [taggsText, setTaggsText] = useState("");
+  const [step, setStep] = useState(0);
 
-    const [taggs, setTaggs] = useState([])
+  const navigate = useNavigate();
 
-    const [links, setLinks] = useState([""]);
+  const [isCategoryOpen, setCategoryOpen] = useState(false);
 
-    const [step, setStep] = useState(0)
+  const [isProfessionOpened, setProfessionOpened] = useState(false);
 
-    const navigate = useNavigate();
+  const [categoryInformation, setCategoryInformation] = useState({
+    category: categorys[0],
+    profession: {},
+  });
 
-    const [isCategoryOpen, setCategoryOpen] = useState(false); 
+  const [errors, setErrors] = useState({
+    descriptionError: false,
+    taggsError: false,
+    linksError: false,
+  });
 
-    const [isProfessionOpened, setProfessionOpened] = useState(false);
+  useEffect(() => {
+    BaidgeButtonConroller.controlText({ step });
+  }, [step]);
 
-    const [categoryInformation, setCategoryInformation] = useState({category : categorys[0], profession : {}})
+  useEffect(() => {
+    const notEmptyLinks = links.filter( (link) => link.length !== 0 )
+    const notEmptyTaggs = links.filter( (tag) => tag.length !== 0 )
+    const lErrors = {
+      descriptionError: false,
+      taggsError: false,
+      linksError: false,
 
-    const [errors, setErrors] = useState({
-        descriptionError : false,
-        taggsError : false,
-        linksError : false
-    })
-
-    useEffect( () => {
-        BaidgeButtonConroller.controlText({step})
-    } , [step] )
-    
-    useEffect( () => {
-        const lErrors = {
-            descriptionError : false,
-            taggsError : false,
-            linksError : false
-        }
-        if (description.length > 500 || description.length < 5 ){
-            lErrors.descriptionError = true
-        }
-        if (links.length > 5){
-            lErrors.linksError = true
-        }
-        setErrors(lErrors)
-    } , [description, links] )
-    
-
-    useEffect( () => {
-        BaidgeButtonConroller.controlVisability({errors})
-    } , [errors] )
-
-    useEffect( () => {
-        const goFoward = BaidgeButtonConroller.forwardFunction({setStep, step, isCategoryOpen, isProfessionOpened});
-        const goBack = BaidgeButtonConroller.backFunction({navigate, step, setStep, isCategoryOpen, isProfessionOpened})
-        MainButton.onClick(goFoward);
-        BackButton.onClick(goBack);
-        return () => {
-            MainButton.offClick(goFoward)
-            BackButton.offClick(goBack)
-        }
-    } , [step, navigate, setStep, isCategoryOpen, isProfessionOpened] )
-
-    useEffect( () => {
-        MainButton.show();
-        BackButton.show();
-        return () => {
-            MainButton.hide();
-            BackButton.hide();
-        }
-    } , [] )
-
-    useEffect( () => {
-        menuController.lowerMenu();
-    } , [] )
-    
-    if (categorys.length === 0 || professions.length === 0){
-        return (
-            <MyLoader />
-        )
+    };
+    if (description.length > 500 || description.length < 5) {
+      lErrors.descriptionError = true;
     }
-    return (
-        <div className={`flex min-w-[200vw] transition-transform duration-300 ${step === 0 ? "translate-x-0" : "-translate-x-[100vw]"}`}>
-            <BaidgeCreaitingOne categoryInformation={categoryInformation} isCategoryOpen={isCategoryOpen} isProfessionOpened={isProfessionOpened} setCategoryInformation={setCategoryInformation} setCategoryOpen={setCategoryOpen} setProfessionOpened={setProfessionOpened}  description={description} setDescription={setDescription} taskInformation={taskInformation} />
-            <BaidgeCreatingTwo links={links} setLinks={setLinks} setTaggs={setTaggs} setTaggsText={setTaggsText} taggs={taggs} taggsText={taggsText} />
-        </div>
-    );
+    if (notEmptyLinks.length > 5) {
+      lErrors.linksError = true;
+    }
+    if (notEmptyTaggs.length > 5){
+        lErrors.taggsError = true
+    }
+    setErrors(lErrors);
+  }, [description, links]);
+
+  useEffect(() => {
+    BaidgeButtonConroller.controlVisability({ errors });
+  }, [errors]);
+
+  useEffect(() => {
+    const goFoward = BaidgeButtonConroller.forwardFunction({
+      setStep,
+      step,
+      isCategoryOpen,
+      isProfessionOpened,
+    });
+    const goBack = BaidgeButtonConroller.backFunction({
+      navigate,
+      step,
+      setStep,
+      isCategoryOpen,
+      isProfessionOpened,
+    });
+    MainButton.onClick(goFoward);
+    BackButton.onClick(goBack);
+    return () => {
+      MainButton.offClick(goFoward);
+      BackButton.offClick(goBack);
+    };
+  }, [step, navigate, setStep, isCategoryOpen, isProfessionOpened]);
+
+  useEffect(() => {
+    MainButton.show();
+    BackButton.show();
+    return () => {
+      MainButton.hide();
+      BackButton.hide();
+    };
+  }, []);
+
+  useEffect(() => {
+    menuController.lowerMenu();
+  }, []);
+
+  if (categorys.length === 0 || professions.length === 0) {
+    return <MyLoader />;
+  }
+  return (
+    <div
+      className={`flex min-w-[200vw] transition-transform duration-300 ${
+        step === 0 ? "translate-x-0" : "-translate-x-[100vw]"
+      }`}
+    >
+      <BaidgeCreaitingOne
+        categoryInformation={categoryInformation}
+        isCategoryOpen={isCategoryOpen}
+        isProfessionOpened={isProfessionOpened}
+        setCategoryInformation={setCategoryInformation}
+        setCategoryOpen={setCategoryOpen}
+        setProfessionOpened={setProfessionOpened}
+        description={description}
+        setDescription={setDescription}
+        taskInformation={taskInformation}
+      />
+      <BaidgeCreatingTwo
+        links={links}
+        setLinks={setLinks}
+        setTaggs={setTaggs}
+        setTaggsText={setTaggsText}
+        taggs={taggs}
+        taggsText={taggsText}
+      />
+    </div>
+  );
 };
 
 export default BaidgeCreating;
