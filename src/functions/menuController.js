372a626f13ecd76a4c7ad24e63972cf3732424f0
SecondNiceee <col1596321@gmail.com>
2025-05-16 20:19:54 +0300

    use Navigate back change

--- src/functions/menuController.js
+++ src/functions/menuController.js
@@ -2,24 +2,24 @@ class MenuController{
     hideMenu(){
         setTimeout( () => {
             document.documentElement.querySelector(".FirstMenu").style.display = "none"
-        }, 200 )
+        }, 0 )
     }
     showMenu(){
         setTimeout( () => {
             document.documentElement.querySelector(".FirstMenu").style.display = "flex"
-        }, 200 )
+        }, 0 )
     }
     lowerMenu(){
         setTimeout( () => {
             document.documentElement.querySelector(".FirstMenu").classList.add("disappearAnimation")
             document.documentElement.querySelector(".FirstMenu").classList.remove("appearAnimation")
-        }, 200 )
+        }, 0 )
     }
     raiseMenu(){
         setTimeout( () => {
             document.documentElement.querySelector(".FirstMenu").classList.add("appearAnimation")
             document.documentElement.querySelector(".FirstMenu").classList.remove("disappearAnimation")
-        }, 200 )
+        }, 0 )
     }
 }
 
