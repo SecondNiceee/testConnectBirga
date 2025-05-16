869709ec994aa7ac7f8ad7b9090a18bbc24f5310
SecondNiceee <col1596321@gmail.com>
2025-05-16 04:31:38 +0300

    fix

--- src/functions/menuController.js
+++ src/functions/menuController.js
@@ -1,18 +1,26 @@
 class MenuController{
     menu = document.documentElement.querySelector(".FirstMenu")
     hideMenu(){
-        this.menu.style.display = "none"
+        if (this.menu){
+            this.menu.style.display = "none"
+        }
     }
     showMenu(){
-        this.menu.style.display = "flex"
+        if (this.menu){
+            this.menu.style.display = "flex"
+        }
     }
     lowerMenu(){
-        this.menu.classList.add("disappearAnimation")
-        this.menu.classList.remove("appearAnimation")
+        if (this.menu){
+            this.menu.classList.add("disappearAnimation")
+            this.menu.classList.remove("appearAnimation")
+        }
     }
     raiseMenu(){
-        this.menu.classList.add("appearAnimation")
-        this.menu.classList.remove("disappearAnimation")
+        if (this.menu){
+            this.menu.classList.add("appearAnimation")
+            this.menu.classList.remove("disappearAnimation")
+        }
     }
 }
 
